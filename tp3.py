cdb5e607849f841b630d0f77084e1b109cf9b49a
KHOUTAIBI <khoutaibi.iliass@gmail.com>
2024-10-26 14:05:27 +0200

    finished wavelet

--- tp3.py
+++ tp3.py
@@ -5,7 +5,8 @@ import matplotlib.pyplot as plt
 np.random.seed(0)
 NUMBER_VARIABLES = 1000
 assert(NUMBER_VARIABLES>4)
-phis = np.random.randint(1,1000,size=(1,1004))
+phis = np.random.randint(1,1000,size=(4))
+print(phis)
 
 def white_noise(mean,sigma,number_variables):
     return np.random.normal(mean,sigma,1_000)
@@ -15,16 +16,28 @@ def AR(p):
     initial_matrix = np.random.randint(1,1000,size=(1,4))
     AR_process[0:3] = initial_matrix[0:3]
     for variable in range(4,NUMBER_VARIABLES+4):
-        AR_process[variable] = phis[variable-1]*AR_process[variable-1] 
-        + phis[variable-2]*AR_process[variable-2]
-        + phis[variable-3]*AR_process[variable-3]
-        + phis[variable-4]*AR_process[variable-4]
+        AR_process[variable] = phis[0]*AR_process[variable-1] 
+        + phis[1]*AR_process[variable-2]
+        + phis[3]*AR_process[variable-3]
+        + phis[4]*AR_process[variable-4]
     return AR_process[4:]
 
 def Gamma(p):
-    pass
+    phi_matrix = np.dot(np.array(
+        [1,-phis[0],-phis[1],-phis[2],-phis[3]]
+    ),
+    np.transpose(
+        np.array([1,-phis[0],-phis[1],-phis[2],-phis[3]])
+            )
+        
+    )
 
+    print(np.array(
+        [1,-phis[0],-phis[1],-phis[2],-phis[3]]
+    ))
+    return phi_matrix
 
+print(Gamma(4))
 
 
 
