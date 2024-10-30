f86ffda03b32d5fd6d1400b4eab09b051dddd162
KHOUTAIBI <khoutaibi.iliass@gmail.com>
2024-10-30 18:07:23 +0100

    finished tp 3, only need to plot the zeros ans poles

--- tp3.py
+++ tp3.py
@@ -1,43 +1,86 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import scipy.linalg as sg
+import scipy.signal as sp
 
 # Initializing the variables here
-np.random.seed(0)
-NUMBER_VARIABLES = 1000
-assert(NUMBER_VARIABLES>4)
-phis = np.random.randint(1,1000,size=(4))
-print(phis)
+NUMBER_VARIABLES = 1_000
+assert NUMBER_VARIABLES>4 
+SIGMA = 2
+PHIS = np.array([0.1,0.2,0.15,0.4])
+print(f"phis are {PHIS}")
 
 def white_noise(mean,sigma,number_variables):
-    return np.random.normal(mean,sigma,1_000)
+    return np.random.normal(mean,sigma,number_variables)
 
 def AR(p):
-    AR_process = np.zeros(NUMBER_VARIABLES+4)
-    initial_matrix = np.random.randint(1,1000,size=(1,4))
-    AR_process[0:3] = initial_matrix[0:3]
-    for variable in range(4,NUMBER_VARIABLES+4):
-        AR_process[variable] = phis[0]*AR_process[variable-1] 
-        + phis[1]*AR_process[variable-2]
-        + phis[3]*AR_process[variable-3]
-        + phis[4]*AR_process[variable-4]
-    return AR_process[4:]
+
+    AR_process = np.random.normal(0,SIGMA,size=NUMBER_VARIABLES)
+    
+    for variable in range(NUMBER_VARIABLES):
+        for shift in range(p):
+            AR_process[variable] += (PHIS[shift] * AR_process[variable-shift-1]) 
+    return AR_process
+
+def empirical_mean(X):
+    assert(len(X) != 0)
+    return np.mean(X)
+
+def empirical_autocovariance(X, taus):
+    N = len(X)
+    X_sum = np.zeros(len(taus))
+    
+    for tau, k in enumerate(taus):
+        X_shifted = X[k:N] - empirical_mean(X)
+        X_original = X[0:N-k] - empirical_mean(X)
+        X_sum[tau] = (1/N)  * np.sum(X_shifted * X_original)
+    
+    return X_sum   
 
 def Gamma(p):
-    phi_matrix = np.dot(np.array(
-        [1,-phis[0],-phis[1],-phis[2],-phis[3]]
-    ),
-    np.transpose(
-        np.array([1,-phis[0],-phis[1],-phis[2],-phis[3]])
-            )
-        
-    )
+    AR_process = AR(p)
+    taus = np.arange(0,p+1)
+    empirical_autocov = empirical_autocovariance(AR_process,taus=taus) 
+    gamma = sg.toeplitz(empirical_autocov)
+    # printing the toeplitz values in order to test the validity of the result
 
-    print(np.array(
-        [1,-phis[0],-phis[1],-phis[2],-phis[3]]
-    ))
-    return phi_matrix
+    approximation = np.linalg.inv(gamma) @ np.array([1,0,0,0,0]).T
+    sigma_approximation = 1/approximation[0]
+    empirical_phis = -(sigma_approximation*approximation[1:])
+    print(f"empirical sigma is {sigma_approximation}")
+    print(f"empirical phis are {empirical_phis}")
 
-print(Gamma(4))
+    MSE = np.sum((PHIS-empirical_phis)**2/(PHIS**2))
+    print(f"the MSE is equal to: {MSE}")
+    
+    return gamma 
+
+def spectral_density(nu,p):
+    AR_process = AR(p)
+    gamma = Gamma(p)
+    approximation = np.linalg.inv(gamma) @ np.array([1,0,0,0,0]).T
+    sigma_approximation = 1/approximation[0]
+    empirical_phis = -(sigma_approximation*approximation[1:])
+    w = 2*np.pi*nu    
+    nu , Sxx = sp.freqz([1],empirical_phis,w)
+    return nu , Sxx
 
 
+def theo_spectral_density(nu):
+    w = 2*np.pi*nu    
+    nu , Sxx = sp.freqz([1],PHIS,w)
+    return nu , Sxx
 
+
+nu = np.linspace(-0.5,0.5,20_000)
+empirical_spectral = spectral_density(nu,4)
+theoretical_spectral = theo_spectral_density(nu)
+
+plt.grid()
+plt.plot(nu,np.abs(empirical_spectral[1]))
+plt.plot(nu,np.abs(theoretical_spectral[1]))
+plt.xlabel("nu")
+plt.ylabel("module")
+plt.legend(["empirical power spectral density","theoretical power spectral density"])
+
+plt.show()
